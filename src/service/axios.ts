d4ac8bf52b8f9791894e16c27569aff9ecd7a373
Julio Filizzola <juliofilizzola@Hotmail.com>
2024-02-07 09:38:31 -0300

    feat: modify base url

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const baseURL = import.meta.env.VITE_BASE_URL;
+const baseURL = import.meta.env.VITE_BASE_URL || 'http://host.docker.internal:3000';
 
 const api = axios.create({
   baseURL,
